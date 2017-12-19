c2306f5d90fc5e3b3c9fd569f2b8dab4d9709ad5
Nicklas Pihl Mathiesen <nick5666@edu.mercantec.dk>
2017-12-19 14:30:48 +0100

    tilføjede til hjemmeside login

---
+++ loginTestphp.php
@@ -0,0 +1,17 @@
+
+	<link rel="stylesheet" type="text/css" href="test.css">
+	<title>Test</title>
+
+<h1>Test</h1>
+
+<div class="box">
+	<div class="secondBox">
+	  <h2>Login:</h2>
+<form action="login.php" method="POST">
+    username:<br><input type="text" name="username" required><br><br>
+    password:<br><input type="password" name="password" required><br><br>
+    <input type="submit" value="Login">
+    <input type='submit' value='sign up'>
+    </div>
+ 	
+</div>
