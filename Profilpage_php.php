096fa51542dce6fde4fdb3863f7cb1330f628837
Morten Larsen <mortenlarsen16@gmail.com>
2017-12-19 17:39:41 +0100

    Tilføjelse af Profilepage

---
+++ Profilpage_php.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+<link rel="stylesheet" type="text/css" href="Profilpage.css">
+
+<head>
+
+	<title></title>
+
+</head>
+
+<body>
+
+	<h1>Profilpage</h1>
+
+	<div class="box">
+	<div class="secondBox"><br>
+	<div>ID</div><br>
+	<div>Gamertag</div><br>
+	<div>Name</div><br>
+	<div>E-mail</div><br>
+	<div>Phonenumber</div><br>
+	<div>Teams</div><br>
+
+</div>
+
+</div>
+
+</body>
+
+</html>
