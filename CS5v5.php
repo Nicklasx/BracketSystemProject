230b2c813bd7fb41edc29a8103a49a2905921d8b
Nicklas Pihl Mathiesen <nick5666@edu.mercantec.dk>
2017-12-20 14:04:35 +0100

    udvikling på cs5v5

--- CS5v5.php
+++ CS5v5.php
@@ -5,7 +5,10 @@ include "header.php";
 <h1>CS5v5</h1>
 
 <div class="box">
-	<div class="secondBox"></div>
+		<div class="tilmeld">
+			<h4>Join turnament</h4>
+			<input type="submit" value="Join with team">
+		</div>
 </div>
 
 <?php
