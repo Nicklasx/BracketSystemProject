b8aff3e71898bbe6f235d5713f7cda83a1c7e04b
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2017-12-20 14:07:51 +0100

    udvikling match

--- Matches.php
+++ Matches.php
@@ -5,20 +5,22 @@ include "header.php";
 <h1>Matches</h1>
 
 <div class="box">
-<div class="secondbox">
 
-	<div>My Matches</div><br>
+
 	
-	<table style="width:100%">
+	<div>My Matches</div><br>
+
+<div class="Test"></div>
+	<table style="border: 2px solid black; width: 300%; background-color: black; margin-left: -375px;">
   <tr>
-    <th>Team</th>
-    <th>VS</th> 
-    <th>Team</th>
+    <th style="color:white;">Team</th>
+    <th style="color:white;">VS</th> 
+    <th style="color:white;">Team</th>
   </tr>
   <tr>
-    <td></td>
-    <td></td> 
-    <td></td>
+    <td style="color:white;">E-sport Vesthimmerland</td>
+    <td style="color:white;">VS</td> 
+    <td style="color:white;">Kolding Hawks</td>
   </tr>
   <tr>
     <td></td>
@@ -27,7 +29,7 @@ include "header.php";
   </tr>
 </table>
 	
-</div>	
+	
 
 </div>
 
