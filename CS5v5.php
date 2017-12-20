df3a6a13d2d5cf9899fd903adace9790967a7c61
Nicklas Pihl Mathiesen <nick5666@edu.mercantec.dk>
2017-12-20 15:45:44 +0100

    cs5v5 done?

--- CS5v5.php
+++ CS5v5.php
@@ -11,21 +11,19 @@ include "header.php";
 			<input type="submit" value="Join with team">
 		</div>
 
-		<div class="b1">Team</div>
+		<div class="b1">Quarterfinal</div>
 <br>
-		<div class="b2">Team</div>
+		<div class="b2">Quarterfinal</div>
 <br>
-		<div class="b3">Team</div>
+		<div class="b3">Quarterfinal</div>
 <br>
-		<div class="b4">Team</div>
-
-		<div class="b5">Team</div>
-
-		<div class="b6">Team</div>
-
-		<div class="b7">Team</div>
-
-		<div class="b8">Team</div>
+		<div class="b4">Quarterfinal</div>
+<br>
+		<div class="b5">Semifinal</div>
+<br>
+		<div class="b6">Semifinal</div>
+<br>
+		<div class="b7">Final</div>
 
 </div>
 
