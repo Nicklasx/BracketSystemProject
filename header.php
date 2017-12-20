a6ef88d42e753e2205e82d89795562ae363698e3
Nicklas Pihl Mathiesen <nick5666@edu.mercantec.dk>
2017-12-20 10:20:35 +0100

    tilføjed nav bar

--- header.php
+++ header.php
@@ -10,3 +10,14 @@ include "sqlconn.php";
 </head>
 
 <body>
+
+<ul class="topnav">
+  <li><a href="index.php">Home</a></li>
+  <li><a href="#news">Turnements</a></li>
+  <li><a href="#contact">Create Team</a></li>
+  <li><a href="#about">My matches</a></li>
+  <li><a href="#about">About</a></li>
+  <li><a href="#about">Profile</a></li>
+  <li><a href="#about">Login</a></li>
+  <li><a href="#about">Logout</a></li>
+</ul>
