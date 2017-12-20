0ede37d8fde0fb1688de75b8e72505393ddfe8d1
Fumlefinger <jann2273@gmail.com>
2017-12-20 08:30:00 +0100

    signup merget med hinanden

--- signupform.php
+++ signupform.php
@@ -17,29 +17,35 @@ if(isset($_GET['error'])) {
 }
 ?>
 
-<h2>Signup Details:</h2>
-<form action="register.php" method="POST">
-    Username: <br><input type="text" name="username" required> <br>
-    Password: <br><input type="password" name="password" required> <br>
-    Confirm Password: <br><input type="password" name="password2" required> <br>
-    E-mail: <br><input type="text" name="e-mail" required> <br>
-    phone number: <br><input type="text" name="phonenumber" required> <br>
-    name: <br><input type="text" name="name" required> <br>
-    last name: <br><input type="text" name="lastname" required> <br>
-    GamerTag: <br><input type="text" name="gamertag" required> <br>
+<h1>Test</h1>
 
-    <h4>Team Captain?</h4>
-    <select name="teamcaptain">
-    	<option value=""></option>
-    	<option value="yes">Yes</option>
-    	<option value="no">No</option>
-    </select>
-    <input type="submit" value="Register">
-</form>
+<div class="box">
+    <div class="secondBox">
+        <h2>Signup Details:</h2>
+        <form action="register.php" method="POST">
+            Username: <br><input type="text" name="username" required> <br>
+            Password: <br><input type="password" name="password" required> <br>
+            Confirm Password: <br><input type="password" name="password2" required> <br>
+            E-mail: <br><input type="text" name="e-mail" required> <br>
+            phone number: <br><input type="text" name="phonenumber" required> <br>
+            name: <br><input type="text" name="name" required> <br>
+            last name: <br><input type="text" name="lastname" required> <br>
+            GamerTag: <br><input type="text" name="gamertag" required> <br>
 
-<form action="index.php" method="POST">
-    <input type="submit" value="Cancel">
-</form>
+            <h4>Team Captain?</h4>
+            <select name="teamcaptain">
+                <option value=""></option>
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+            </select>
+            <input type="submit" value="Register">
+        </form>
+
+        <form action="index.php" method="POST">
+            <input type="submit" value="Cancel">
+        </form>
+    </div>
+</div>
 
 
 <?php
