c2a61c5666b3337a3495c9cb5c5f8690ad3d49c7
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2017-12-21 12:43:57 +0100

    udvikling

--- Profilpage_php.php
+++ Profilpage_php.php
@@ -3,14 +3,18 @@ include "header.php";
 ?>
 	<h1>Profilpage</h1>
 
+	<form action="teamregister.php" method="POST">
+
 	<div class="box">
 	<div class="secondBox"><br>
-	<div>ID</div><br>
-	<div>Gamertag</div><br>
-	<div>Name</div><br>
-	<div>E-mail</div><br>
-	<div>Phonenumber</div><br>
-	<div>Teams</div><br>
+	<div>ID:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Gamertag:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Name:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>E-mail:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Phonenumber:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Teams:</div><br><input type="text" name="teamName" required> <br><br>
+
+</form>
 
 </div>
 
