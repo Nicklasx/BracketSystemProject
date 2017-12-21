e4cf0702b2ba839c0e2255dd11baf9599fc85ee2
Fumlefinger <jann2273@gmail.com>
2017-12-21 12:49:50 +0100

    yo

--- teaminsert.php
+++ teaminsert.php
@@ -1,6 +1,7 @@
 <?php
 $line = "";
 $counter = 0;
+$decide = 0;
 include "header.php";
 
 $sql = "SELECT * FROM `hold` where `HoldNavn` = '".$_POST['nytnavn']."'";
@@ -36,8 +37,48 @@ if ($conn->query($sql) === TRUE ) {
 $sql = "SELECT * FROM `tournering` WHERE `ID` = '".$_GET['id']."'";
 $result = $conn->query($sql);
 while($row = $result->fetch_assoc()) {
-    $counter++;
+    
+    $counter1 = explode(",",$row['match1']);
+    $counter2 = explode(",",$row['match2']);
+    $counter3 = explode(",",$row['match3']);
+    $counter4 = explode(",",$row['match4']);
+    if (count($counter1) < 2 ) {
+        $decide = 1;
+    }
+    else if (count($counter2) < 2) {
+        $decide = 2;
+    }
+    else if (count($counter3) < 2 ) {
+        $decide = 3;
+    }
+    else if (count($counter4) < 2) {
+        $decide = 4;
+    }
+    else {
+        $decide = 1;
+    }
+    $line2 ="";
+    if (!empty($row['match'.$decide])) {
+        $line2 = $row['match'.$decide];
+    }
+    
 }
+if (empty($line2)) {
+    $line2 = $_POST['nytnavn'];
+}
+else {
+    $line2 = ",".$_POST['nytnavn'];
+}
+
+$sql = "UPDATE `tournering` SET `match".$decide."`='".$line2."' WHERE ID = '".$id."'";
+if ($conn->query($sql) === TRUE ) {
+    $error = 1;
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
 ?>
 
 
