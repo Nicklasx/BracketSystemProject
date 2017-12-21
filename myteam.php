711e92772b6e8f2ff724bf9a1938723c9a00d8b4
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2017-12-21 13:26:39 +0100

    udvikling

---
+++ myteam.php
@@ -0,0 +1,24 @@
+<?php
+include "header.php";
+?>
+
+<h1>My Team</h1>
+
+	<form action="teamregister.php" method="POST">
+
+	<div class="box">
+	<div class="secondBox"><br>
+	<div>Team Name:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Player:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Player:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Player:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Player:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Player:</div><br><input type="text" name="teamName" required> <br><br>
+	<div>Player:</div><br><input type="text" name="teamName" required> <br><br>
+
+
+</form>
+
+
+<?php
+include "footer.php";
